f9246351aad6d736c5b3a86b811c38e3bd9fbf5d
AgipStephane <smalho@aexis.com>
2026-02-17 23:37:40 +0100

    fix quality image

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        qualities: [50, 60, 65, 70, 75, 80],
+    },
     async redirects() {
         return [
             {
